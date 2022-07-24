bec6413b9ede835c16d7730b6e57157410f25866
usbTypeF <81485520+usbTypeF@users.noreply.github.com>
2022-07-24 00:52:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var number = 0;
+document.body.style.cursor = 'none';
 
 function asdf()
 {
@@ -6,3 +7,29 @@ function asdf()
     console.log(number);
     document.getElementById("number").innerHTML = number;
 }
+
+function draw() {
+    const canvas = document.getElementById("mouse");
+    canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+    const ctx = canvas.getContext("2d");
+      
+  
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+}
+
+function updatemouse()
+{
+    const canvas = document.getElementById("mouse");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var x = event.clientX;
+    var y = event.clientY;
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+draw();
