131815d0e954e2ea46fa27fa204f25b248229bdb
usbTypeF <81485520+usbTypeF@users.noreply.github.com>
2022-07-25 00:34:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
 var number = 0;
-document.body.style.cursor = 'none';
+var plus, minus = 1;
+var b, a = 1;
+document.getElementById("test").innerHTML = number;
+document.getElementById("test2").innerHTML = minus;
+document.getElementById("test3").innerHTML = a;
 
 function asdf()
 {
-    number++;
-    console.log(number);
-    document.getElementById("number").innerHTML = number;
+    plus = a * minus;
+    number += plus;
+    document.getElementById("test").innerHTML = number.toFixed(1);
 }
 
-function draw() {
-    const canvas = document.getElementById("mouse");
-    canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-    const ctx = canvas.getContext("2d");
-      
-  
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(45, 45, 60, 60);
-    ctx.strokeRect(50, 50, 50, 50);
+function add()
+{
+    minus += number / 100
+    number = 0;
+    document.getElementById("test").innerHTML = number.toFixed(1);
+    document.getElementById("test2").innerHTML = minus.toFixed(1);
+}
+
+function big()
+{
+    if(number >= 10)
+    {
+        a += Math.log10(number) - 1;
+        number = 0;
+        document.getElementById("test").innerHTML = number.toFixed(1);
+        document.getElementById("test2").innerHTML = minus.toFixed(1);
+        document.getElementById("test3").innerHTML = a.toFixed(1);
+    }
 }
 
 function updatemouse()
 {
-    const canvas = document.getElementById("mouse");
+    const canvas = document.getElementById("game");
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var x = event.clientX;
     var y = event.clientY;
-    ctx.beginPath();
+    ctx.fillRect((x - window.height), (y - screen.height * 0.2)/4, 10, 10);
+    /*ctx.beginPath();
     ctx.arc(x, y, 15, 0, 2 * Math.PI);
     ctx.fillStyle = "grey";
     ctx.fill();
     ctx.lineWidth = 2;
-    ctx.stroke();
+    ctx.stroke();*/
 }
-
-draw();
